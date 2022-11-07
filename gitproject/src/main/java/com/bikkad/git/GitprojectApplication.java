9502357b3720eb8dafb355b2d36e3ebb4a8ecd2a
amitpathade <amitpathadepatil@gmail.com>
2022-11-07 11:51:15 +0530

    ok

---
+++ gitproject/src/main/java/com/bikkad/git/GitprojectApplication.java
@@ -0,0 +1,13 @@
+package com.bikkad.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitprojectApplication.class, args);
+	}
+
+}
---
+++ gitproject/src/test/java/com/bikkad/git/GitprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikkad.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
